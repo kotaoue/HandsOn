0d04f6cb372b0fa77df5c3eb485c94bf15e11ed7
kotaoue <kotaoue@gmail.com>
2026-05-12 06:35:09 +0900

    Create 1-3.sql

---
+++ SQLAntipatterns/1-3.sql
@@ -0,0 +1,10 @@
+create table Accounts (
+    account_id SERIAL PRIMARY KEY,
+    account_name VARCHAR(20),
+    first_name VARCHAR(20),
+    last_name VARCHAR(20),
+    email VARCHAR(100),
+    password_hash CHAR(64),
+    portrait_image BLOB,
+    hourly_rate NUMERIC(9, 2)
+);

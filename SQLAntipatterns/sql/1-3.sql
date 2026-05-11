95f563da4025d111fdd82d57cd5795d179290daf
kotaoue <kotaoue@gmail.com>
2026-05-12 06:50:23 +0900

    Update 1-3.sql

--- SQLAntipatterns/sql/1-3.sql
+++ SQLAntipatterns/sql/1-3.sql
@@ -41,4 +41,13 @@ create table if not exists Comments (
     FOREIGN KEY (author) REFERENCES Accounts(account_id)
 );
 
+create table if not exists Screenshots (
+    bug_id BIGINT UNSIGNED NOT NULL,
+    image_id BIGINT UNSIGNED NOT NULL,
+    screenshot_image BLOB,
+    caption VARCHAR(100),
+    PRIMARY KEY (bug_id, image_id),
+    FOREIGN KEY (bug_id) REFERENCES Bugs(bug_id)
+);
+
 show tables;

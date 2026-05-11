00633e083fa247a074b65c574418e82b94604394
kotaoue <kotaoue@gmail.com>
2026-05-12 06:46:57 +0900

    Update 1-3.sql

--- SQLAntipatterns/sql/1-3.sql
+++ SQLAntipatterns/sql/1-3.sql
@@ -13,4 +13,22 @@ create table if not exists BugStatus (
     status VARCHAR(20) PRIMARY KEY
 );
 
+create table if not exists Bugs (
+    bug_id SERIAL PRIMARY KEY,
+    data_reported DATE NOT NULL DEFAULT (CURRENT_DATE),
+    summary VARCHAR(80),
+    description VARCHAR(1000),
+    resolution VARCHAR(1000),
+    reported_by BIGINT UNSIGNED NOT NULL,
+    assigned_to BIGINT UNSIGNED,
+    verified_by BIGINT UNSIGNED,
+    status VARCHAR(20),
+    priority VARCHAR(20),
+    hours NUMERIC(9, 2),
+    FOREIGN KEY (reported_by) REFERENCES Accounts(account_id),
+    FOREIGN KEY (assigned_to) REFERENCES Accounts(account_id),
+    FOREIGN KEY (verified_by) REFERENCES Accounts(account_id),
+    FOREIGN KEY (status) REFERENCES BugStatus(status)
+);
+
 show tables;

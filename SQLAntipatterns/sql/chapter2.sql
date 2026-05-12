0d14acbba61e9a510e89aab567c1ebc2cfa4c598
kotaoue <kotaoue@gmail.com>
2026-05-12 13:15:28 +0900

    Update chapter2.sql

--- SQLAntipatterns/sql/chapter2.sql
+++ SQLAntipatterns/sql/chapter2.sql
@@ -36,35 +36,3 @@ desc Products;
 
 insert into Products (product_id, product_name, account_id) values (DEFAULT, 'Visual TurboBuilder Pro', '12,34');
 select * from Products;
-
--- Recreate the table from Chapter 1
-drop table if exists Products;
-
-create table if not exists Products (
-    product_id SERIAL PRIMARY KEY,
-    product_name VARCHAR(50)
-);
-
-create table if not exists Accounts (
-    account_id SERIAL PRIMARY KEY,
-    account_name VARCHAR(20),
-    first_name VARCHAR(20),
-    last_name VARCHAR(20),
-    email VARCHAR(100),
-    password_hash CHAR(64),
-    portrait_image BLOB,
-    hourly_rate NUMERIC(9, 2)
-);
-
-create table if not exists BugProducts (
-    bug_id BIGINT UNSIGNED NOT NULL,
-    product_id BIGINT UNSIGNED NOT NULL,
-    PRIMARY KEY (bug_id, product_id),
-    FOREIGN KEY (bug_id) REFERENCES Bugs(bug_id),
-    FOREIGN KEY (product_id) REFERENCES Products(product_id)
-);
-
-desc Products;
-select * from Products;
-desc BugProducts;
-select * from BugProducts;

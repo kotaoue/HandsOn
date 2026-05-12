076a84352fa8e4bf9edf8ba253f836bbf88ea36f
kotaoue <kotaoue@gmail.com>
2026-05-12 12:57:48 +0900

    Update chapter2.sql

--- SQLAntipatterns/sql/chapter2.sql
+++ SQLAntipatterns/sql/chapter2.sql
@@ -1,4 +1,15 @@
 delete table if exists Products;
+create table if not exists Products (
+    product_id SERIAL PRIMARY KEY,
+    product_name VARCHAR(1000),
+    account_id BIGINT UNSIGNED,
+    FOREIGN KEY (account_id) REFERENCES Accounts(account_id)
+);
+desc Products;
+
+insert into products (product_name, account_id) values (DEFAULT, 'Visual TurboBuilder', 12);
+
+select * from Products;
 
 
 -- Recreate the table from Chapter 1

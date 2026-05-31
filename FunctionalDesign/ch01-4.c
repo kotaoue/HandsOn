932f6182c5ab85003b20ca11414a3a94f9536578
kotaoue <kotaoue@gmail.com>
2026-05-31 10:12:45 +0900

    Create ch01-4.c

---
+++ FunctionalDesign/ch01-4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int sumFirstTenSquaresHelper(int sum, int i)
+{
+    if (i > 10)
+    {
+        return sum;
+    }
+    printf("i: %d, sum: %d\n", i, sum);
+    return sumFirstTenSquaresHelper(sum + i * i, i + 1);
+}
+
+int sumFirstTenSquares()
+{
+    return sumFirstTenSquaresHelper(0, 1);
+}
+
+int main()
+{
+    sumFirstTenSquares();
+}

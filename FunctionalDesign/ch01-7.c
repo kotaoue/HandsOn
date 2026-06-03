1055489fc198cfbefa75213039a63c16a68825b0
kotaoue <kotaoue@gmail.com>
2026-06-03 23:34:34 +0900

    Update ch01-7.c

--- FunctionalDesign/ch01-7.c
+++ FunctionalDesign/ch01-7.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 
-int sumFirstTenSquaresHelper(int sum, int i)
+int sumFirstTenSquaresHelper(void)
 {
-    printf("i: %d, sum: %d\n", i, sum);
-    return (i > 10) ? sum : sumFirstTenSquaresHelper(sum + i * i, i + 1);
+    return (1 > 10)    ? 0
+           : (2 > 10)  ? 1 * 1
+           : (3 > 10)  ? 1 * 1 + 2 * 2
+           : (4 > 10)  ? 1 * 1 + 2 * 2 + 3 * 3
+           : (5 > 10)  ? 1 * 1 + 2 * 2 + 3 * 3 + 4 * 4
+           : (6 > 10)  ? 1 * 1 + 2 * 2 + 3 * 3 + 4 * 4 + 5 * 5
+           : (7 > 10)  ? 1 * 1 + 2 * 2 + 3 * 3 + 4 * 4 + 5 * 5 + 6 * 6
+           : (8 > 10)  ? 1 * 1 + 2 * 2 + 3 * 3 + 4 * 4 + 5 * 5 + 6 * 6 + 7 * 7
+           : (9 > 10)  ? 1 * 1 + 2 * 2 + 3 * 3 + 4 * 4 + 5 * 5 + 6 * 6 + 7 * 7 + 8 * 8
+           : (10 > 10) ? 1 * 1 + 2 * 2 + 3 * 3 + 4 * 4 + 5 * 5 + 6 * 6 + 7 * 7 + 8 * 8 + 9 * 9
+                       : 1 * 1 + 2 * 2 + 3 * 3 + 4 * 4 + 5 * 5 + 6 * 6 + 7 * 7 + 8 * 8 + 9 * 9 + 10 * 10;
 }
 
 int sumFirstTenSquares()
 {
-    return sumFirstTenSquaresHelper(0, 1);
+    return sumFirstTenSquaresHelper();
 }
 
 int main()

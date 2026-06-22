ca7babb59c69ead34bc1ef101a60075b293ccd71
kotaoue <kotaoue@gmail.com>
2026-06-22 23:28:47 +0900

    Create ch03-5.clj

---
+++ FunctionalDesign/ch03-5.clj
@@ -0,0 +1,15 @@
+; 再帰
+(defn ifib ([n a b]
+  (if (= 0 n)
+    b
+    (recur (dec n) b (+ a b)))))
+
+([n]
+  (cond
+    (< n 1) nil
+    (<= n 2) 1
+    :else (map ifib (- n 2) 1 1)
+  )
+)
+
+(println (ifib 30 1 1))

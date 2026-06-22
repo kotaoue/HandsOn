19d502faff3216a59e213d340b76f828478dd4f3
kotaoue <kotaoue@gmail.com>
2026-06-22 23:43:16 +0900

    Update ch04-1.clj

--- FunctionalDesign/ch04-1.clj
+++ FunctionalDesign/ch04-1.clj
@@ -8,7 +8,7 @@
 
 (def fib (memoize fib-w))
 
-(defn lazy-fibs [n]
+(defn lazy-fibs []
   (map fib (rest (range))))
 
 (take 10 (lazy-fibs))
@@ -18,4 +18,4 @@ returns: (1 1 2 3 5 8 13 21 34 55)
 returns: (1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584 4181 6765 10946 17711 28657 46368 75025 121393 196418 317811 514229)
 
 (println (fib 30))
-(println (lazy-fibs 30))
+(println (take 30 (lazy-fibs)))

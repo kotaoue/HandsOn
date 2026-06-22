30ecf2f000ab4a72c342aa44241381dcfde80c3f
kotaoue <kotaoue@gmail.com>
2026-06-22 23:42:34 +0900

    Create ch04-1.clj

---
+++ FunctionalDesign/ch04-1.clj
@@ -0,0 +1,21 @@
+(declare fib)
+
+(defn fib-w [n]
+  (cond
+    (< n 1) nil
+    (<= n 2) 1
+    :else (+ (fib (dec n)) (fib (- n 2)))))
+
+(def fib (memoize fib-w))
+
+(defn lazy-fibs [n]
+  (map fib (rest (range))))
+
+(take 10 (lazy-fibs))
+returns: (1 1 2 3 5 8 13 21 34 55)
+
+(ntn (lazy-fibs) 30)
+returns: (1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584 4181 6765 10946 17711 28657 46368 75025 121393 196418 317811 514229)
+
+(println (fib 30))
+(println (lazy-fibs 30))

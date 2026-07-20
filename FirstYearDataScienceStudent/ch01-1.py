788c9dcb3ca8e483278289750bd2af39b3199709
kotaoue <1369749+kotaoue@users.noreply.github.com>
2026-07-20 15:05:08 +0900

    Update ch01-1.py

--- FirstYearDataScienceStudent/ch01-1.py
+++ FirstYearDataScienceStudent/ch01-1.py
@@ -5,9 +5,11 @@ import seaborn as sns
 sns.set_theme()
 plt.rcParams["font.family"] = "Hiragino Sans"
 
-df = pd.DataFrame({"値段": [100, 120, 110, 130, 150]})
+df = pd.DataFrame(
+    {"値段": [100, 120, 110, 130, 150], "日付": pd.date_range("2023-01-01", periods=5)}
+)
 
-ax = df.plot()
+ax = df.plot(x="日付", y="値段")
 ax.set_title("値段の推移")
 
 plt.show()

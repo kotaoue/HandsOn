db6a96c0416be26e11732c4cfebf93bc1a627bd8
kotaoue <1369749+kotaoue@users.noreply.github.com>
2026-07-20 14:57:48 +0900

    Update ch01-1.py

--- FirstYearDataScienceStudent/ch01-1.py
+++ FirstYearDataScienceStudent/ch01-1.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import japanize_matplotlib
 
+sns.set(font="IPAexGothic")
 df = pd.DataFrame({"値段": [100, 120, 110, 130, 150]})
 df.plot()
 plt.title("値段の推移")

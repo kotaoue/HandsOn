71672f3977486af44a34e93e8fb2a9c172837c40
kotaoue <1369749+kotaoue@users.noreply.github.com>
2026-07-20 15:05:30 +0900

    Update ch01-1.py

--- FirstYearDataScienceStudent/ch01-1.py
+++ FirstYearDataScienceStudent/ch01-1.py
@@ -9,7 +9,7 @@ df = pd.DataFrame(
     {"値段": [100, 120, 110, 130, 150], "日付": pd.date_range("2023-01-01", periods=5)}
 )
 
-ax = df.plot(x="日付", y="値段")
+ax = df.plot(x="日付", y="値段", marker="o", linestyle="-", color="green")
 ax.set_title("値段の推移")
 
 plt.show()
